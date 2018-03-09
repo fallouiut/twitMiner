429e6226c199e0baf75cf8ddc114fa1955022ccd
fallouiut <fallou.seye@etu.univ-amu.Fr>
2018-03-09 13:56:35 +0100

    getTweetsByHashTag

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
 import twitter4j.*;
 import twitter4j.api.DirectMessagesResources;
 import twitter4j.auth.AccessToken;
@@ -5,30 +6,24 @@ import twitter4j.auth.AccessToken;
 import java.util.List;
 
 public class Main {
-    public static void main(String args[]) throws TwitterException {
+    public static void main(String args[]){
 
-        // api key: etTdQUBxUMRC2RqGAq1yt6Yw8
+        TwittterService tws = new TwittterService();
 
-        // Consumer Key (API Key)       etTdQUBxUMRC2RqGAq1yt6Yw8
-        // Consumer Secret (API Secret) e7bK03FK0reXT1kVauQeraj0T8dg5AGvaQy35Ook1lTEEtwUbu
-        // Access Token        969509345313591296-PrpHRN2mznGhsk3o3otyPtnovQDjKyY
-        // Access Token Secret 2o7adEgNULFJ9aHe1QuSpfmY0rNyT0sJ1onMbfnw1RXxJ
-        // Owner Seye38738676
-        // Owner ID 969509345313591296
-        String consumerKey = "etTdQUBxUMRC2RqGAq1yt6Yw8";
-        String consumerSecret = "e7bK03FK0reXT1kVauQeraj0T8dg5AGvaQy35Ook1lTEEtwUbu";
-        String accessToken = "969509345313591296-PrpHRN2mznGhsk3o3otyPtnovQDjKyY";
-        String accessTockenSecret = "2o7adEgNULFJ9aHe1QuSpfmY0rNyT0sJ1onMbfnw1RXxJ";
+        int compteur = 0;
+        try{
+            for(Status s: tws.getByHashTag("OMBilbao")) {
+                System.out.println("@" + s.getUser().getName() + ": " + s.getText() + " / " + s.getGeoLocation());
+                ++compteur;
+            }
 
+            System.out.println("Il y a eu: " + compteur + " tweets concernant ce hashtag");
 
-        Twitter twitter = new TwitterFactory().getInstance();
-        twitter.setOAuthConsumer(consumerKey, consumerSecret);
-        twitter.setOAuthAccessToken(new AccessToken(accessToken, accessTockenSecret));
-
-        List<Status> statuses = twitter.tweets().lookup();
-
-        for(Status s: statuses)
-            System.out.println("Un Status: " + s.getText());
-
-    }
+        }
+        catch(TwitterException te){
+            System.out.println("Exception Twitter: ");
+            te.getMessage();
+            te.printStackTrace();
+        }
+   }
 }
